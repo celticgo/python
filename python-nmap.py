aaa21bfba78f7287d74c7947c17360ade52557ed
GoddessofDirt <76707203+celticgo@users.noreply.github.com>
2024-11-09 15:01:53 -0500

    Update python-nmap.py

--- python-nmap.py
+++ python-nmap.py
@@ -1,62 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[9]:
-
-
 import nmap
-
-
-# In[ ]:
-
-
-
-
-
-# In[16]:
-
 
 nm = nmap.PortScanner()
 
-
-# In[ ]:
-
-
-
-
-
-# In[17]:
-
-
 target = "64.13.134.52"
-
-
-# In[18]:
-
-
 options = "-sV -sC scan_results"
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 nm.scan(target, arguments=options)
-
-
-# In[ ]:
-
-
-
-
-
-# In[20]:
-
 
 for host in nm.all_hosts():
     print("Host: %s (%s)" % (host, nm[host].hostname()))
@@ -67,14 +19,6 @@ for host in nm.all_hosts():
         for port, state in port_info.items():
             print("Port: %s\tState: %s" % (port, state))
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
