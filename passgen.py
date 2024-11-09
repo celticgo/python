c07893807075226f53fc006dd4ce5ac602fb334d
GoddessofDirt <76707203+celticgo@users.noreply.github.com>
2024-11-09 12:54:00 -0500

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,47 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import random
-
-
-# In[2]:
-
-
 import string
-
-
-# In[ ]:
-
-
-
-
-
-# In[3]:
-
 
 def generate_password(length: int = 10):
     alphabet = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(alphabet) for i in range(length))
     return password
 
-
-# In[4]:
-
-
 password = generate_password()
-
-
-# In[5]:
-
-
 print(f"Generated password: {password}")
-
-
-# In[ ]:
-
-
-
 
