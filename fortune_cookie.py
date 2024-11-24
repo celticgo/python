f0dc2d57fb41d44ce1d6beab4e47e9011ea191e7
GoddessofDirt <76707203+celticgo@users.noreply.github.com>
2024-11-24 16:04:10 -0500

    Add files via upload
    file add

---
+++ fortune_cookie.py
@@ -0,0 +1,18 @@
+import random
+
+# random module from w3 schools can give more information about random module
+#magic 8 ball
+
+lucky_number = random.randint(1,100)
+
+fortune_number = random.randint(1,3)
+
+fortune_text = ''
+if fortune_number == 1:
+    fortune_text = 'You will find your best friend today! Adopt a dog from your local shelter!'
+if fortune_number == 2:
+    fortune_text = 'A see fur and paws in your future. A dog is waiting for you at the shelter!'
+if fortune_number== 3:
+    fortune_text = 'You will adopt the best dog! Go visit the shelter today!'
+
+print(f'{fortune_text} The number of dogs you should adopt is {lucky_number}')
